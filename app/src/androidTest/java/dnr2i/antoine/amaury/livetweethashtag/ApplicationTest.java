61923586ead359f3f3ce2367c0da4d8b54bd9081
amaury lavieille <amaury.lavieille@gmail.com>
2015-01-30 16:46:28 +0100

    Initial commit

---
+++ app/src/androidTest/java/dnr2i/antoine/amaury/livetweethashtag/ApplicationTest.java
@@ -0,0 +1,13 @@
+package dnr2i.antoine.amaury.livetweethashtag;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
